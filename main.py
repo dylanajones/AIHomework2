f4303349b6dd4562c169165b310ca19101ef5754
dylanajones <jonesadylan@gmail.com>
2016-01-27 10:14:25 -0800

    Adding in Function headers

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-print "Starting"
-beam_widths = [5,10,15,20,25,50,100,'inf']
-num_h = 2
-problem_size = [4,6,8,10]
-NMAX = 1000000
-
-
 #for i in beam_widths:
 #    for j in range(num_h):
 #        for k in problem_size:
@@ -15,6 +8,9 @@ NMAX = 1000000
 
 def search(start_state, goal_state):
     print "Not done yet"
+    # Need to make queues here and pop / push from them
+    # Also need to design huerisitic functions
+
     return
 
 def make_goal(num_size):
@@ -23,6 +19,22 @@ def make_goal(num_size):
         goal[0].insert(0,i)
 
     return goal
+
+def expand(current_state):
+    return [[],[],[]]
+
+def h1(current_state, goal_state):
+    return 0
+
+def h2(current_state, goal_state):
+    return 0
+
+
+print "Starting"
+beam_widths = [5,10,15,20,25,50,100,'inf']
+num_h = 2
+problem_size = [4,6,8,10]
+NMAX = 1000000
 
 print "This should have 5"
 print make_goal(5)
